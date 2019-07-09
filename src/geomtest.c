fd3802de233b90318a14dca82f9544cb6b083a34
Chris <chrismjboyle@gmail.com>
2019-07-09 16:51:55 +0100

    geomtest updated for batch running

--- src/geomtest.c
+++ src/geomtest.c
@@ -7,9 +7,26 @@ int main (int argc, const char **argv)
 {
   Sim *sim = alloc_sim();
 
-  populate(sim, 10);
+  Particle *p1 = alloc_particle();
+  cvec_set_constant(p1->position, NDIM, 0.0);
+  add_particle(sim, p1);
 
-  fprintf(stderr, "(%f, %f, %f)  (%f, %f, %f)\n", EXPAND(sim->particles[0]->position), EXPAND(sim->particles[9]->position));
+  Particle *p2 = alloc_particle();
+  cvec_set_constant(p2->position, NDIM, 1.0);
+  add_particle(sim, p2);
+
+  cvec_float dr = get_distance(sim, p1, p2);
+
+  if (dr != 0.0) {
+    cvec_test_fail(
+        "geometry test", 
+        "did not calculate distance between two particles in the same position correctly");
+  }
+  else {
+    cvec_test_pass(
+        "geometry test", 
+        "calculated distance between two particles in the same position correctly!");
+  }
 
   free_sim(sim);
 
